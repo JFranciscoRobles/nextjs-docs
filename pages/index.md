de388fb7e78bc62e78603455b015f6b014874603
JFranciscoRobles <josefrancisco.roblesacosta@gmail.com>
2021-04-12 08:10:07 -0700

    update

--- pages/index.md
+++ pages/index.md
@@ -1,7 +1,3 @@
-# Nextra
+# Hackademy Red-Social-API
 
-**Nextra** is a [Next.js](https://nextjs.org) based static site generator. 0 lines of code needed.  
-
-It supports Markdown with React components ([MDX](/mdx)), automatically generated [sidebar and anchor links](/docs/anchors), file-system based routing, built-in syntax highlighting, i18n and more.
-
-![](/demo.png)
+**Documentacion de la API** favor de visitar la pagina.                  
--- theme.config.js
+++ theme.config.js
@@ -1,11 +1,11 @@
 export default {
-  repository: 'https://github.com/shuding/nextra',
+  repository: 'https://github.com/JFranciscoRobles/nextjs-docs',
   titleSuffix: ' – Nextra',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold hidden md:inline">Api</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next Site Builder
+        Hackademy-Red-Social
       </span>
     </>
   ),
@@ -24,12 +24,36 @@ export default {
       <meta name="og:title" content="Nextra: Next.js static site generator" />
       <meta name="og:image" content="https://nextra.vercel.app/og.png" />
       <meta name="apple-mobile-web-app-title" content="Nextra" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png"/>
-      <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png"/>
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-      <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png"/>
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-      <meta name="msapplication-TileImage" content="/ms-icon-144x144.png"/>
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-icon-180x180.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="192x192"
+        href="/android-icon-192x192.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="96x96"
+        href="/favicon-96x96.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
     </>
   ),
   search: true,
@@ -37,5 +61,5 @@ export default {
   nextLinks: true,
   footer: true,
   footerEditOnGitHubLink: true,
-  footerText: <>MIT {new Date().getFullYear()} © Nextra.</>
+  footerText: <>MIT {new Date().getFullYear()} © Harpy.</>,
 }
